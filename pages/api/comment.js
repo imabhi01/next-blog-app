47d053a403ba2736b4c7c8aefee40fe7e222edbb
imabhi01 <iamdev.abhishek@gmail.com>
2022-03-24 16:42:05 +0545

    pus

--- pages/api/comment.js
+++ pages/api/comment.js
@@ -16,7 +16,6 @@ import { GraphQLClient, gql } from 'graphql'
 import { GraphQLClient } from 'graphql-request'
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
-
 export default function comments(req, res){
   const GraphQLClient = new GraphQLClient(graphqlAPI, {
     headers: {
