c01d86441f6269a022048c321540e0bb8a4c264e
imabhi01 <iamdev.abhishek@gmail.com>
2022-04-04 22:39:16 +0545

    comments

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -33,6 +33,7 @@ const CommentsForm = ({slug}) => {
       localStorage.removeItem('email', 'email');
     }
   }
+  
   return (
     <div class="bg-white rounded-lg shadow-lg p-8 mb-8">
       <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
