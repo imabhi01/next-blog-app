05de27a56065dbb80f83151d147720bc77dac017
imabhi01 <iamdev.abhishek@gmail.com>
2022-04-08 21:59:38 +0545

    push

--- pages/api/comment.js
+++ pages/api/comment.js
@@ -11,10 +11,8 @@
 // ) {
 //   res.status(200).json({ name: 'John Doe' })
 // }
-
 import { GraphQLClient, gql } from 'graphql'
 import { GraphQLClient } from 'graphql-request'
-
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 export default function comments(req, res){
   const GraphQLClient = new GraphQLClient(graphqlAPI, {
